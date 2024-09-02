b936611e4b2ecd6ce8bee8be0c0e8634335ff1c2
jusotoTEC <102713314+jusotoTEC@users.noreply.github.com>
2024-09-02 16:47:44 -0600

    Delete Experiment_1/Matlab_Code directory

--- Experiment_1/Matlab_Code/tFrobNorm.m
+++
@@ -1,5 +0,0 @@
-function y=tFrobNorm(X)
-    %Frobeniuns norm of a third-order tensor
-    A=X.^2;
-    y=sqrt(sum(sum(sum(A))));
-end
--- Experiment_1/Matlab_Code/teye.m
+++
@@ -1,6 +0,0 @@
-function I=teye(m,p)
-
-    I=zeros(m,m,p);
-    I(:,:,1)=eye(m);
-
-end
