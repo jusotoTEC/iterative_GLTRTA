f8c44045bf5b5ccd54cb762f091054f3a4339269
Pablo Soto-Quiros <102713314+jusotoTEC@users.noreply.github.com>
2025-07-24 18:04:52 -0600

    Update file_example1.m

--- Numerical Experiment 1/file_example1.m
+++ Numerical Experiment 1/file_example1.m
@@ -1,7 +1,7 @@
 % Numerical Experiment 1
 
 % Reference:
-%   Paper   = "A modified proximal point algorithm for solving a 
+%   Paper   = "A proximal Gauss-Seidel algorithm for solving a  
 %              generalized low-tubal-rank tensor approximation problem 
 %              based on the t-product"
 %   Author = Soto-Quiros, Pablo (jusoto@tec.ac.cr)
